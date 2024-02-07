00b1d35a19374662e74bb3740413e1dcfd2b9879
Abhijith Subash <abhijithsubash07@gmail.com>
2024-02-07 22:15:49 +0530

    feat: product model added (without image)

--- api/models.py
+++ api/models.py
@@ -1,3 +1,14 @@
 from django.db import models
 
-# Create your models here.
+
+class Products(models.Model):
+    name = models.CharField(max_length=200)
+    price = models.FloatField()
+    text = models.CharField(max_length=500)
+    #image
+    MEAL_TYPES = [
+        ("dinner","dinner"),
+        ("breakfast","breakfast"),
+        ("lunch", "lunch")
+    ]
+    type = models.CharField(max_length=10, choices=MEAL_TYPES)
