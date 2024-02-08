b112a72659827d2b5fb76089e2c2e2afcd52b2b0
Abhijith Subash <abhijithsubash07@gmail.com>
2024-02-08 23:37:23 +0530

    fix: serializer added as view

--- api/views.py
+++ api/views.py
@@ -1,53 +1,9 @@
-from django.shortcuts import render
-from django.http import JsonResponse
-from django.views.decorators.csrf import csrf_exempt
+from rest_framework.permissions import DjangoModelPermissionsOrAnonReadOnly
+from rest_framework.generics import ListAPIView
+from .models import Product
+from .serializers import ProductSerializer
 
-
-@csrf_exempt
-def index(request):
-    foodData = [
-        {
-            "name": "Boiled Egg",
-            "price": 10,
-            "text": "Lorem ipsum dolor sit amet consectetur adipisicing elit. Quisquam, voluptatum.",
-            "image": "/images/egg.png",
-            "type": "breakfast",
-        },
-        {
-            "name": "RAMEN",
-            "price": 25,
-            "text": "Lorem ipsum dolor sit amet consectetur adipisicing elit. Quisquam, voluptatum.",
-            "image": "/images/ramen.png",
-            "type": "lunch",
-        },
-        {
-            "name": "GRILLED CHICKEN",
-            "price": 45,
-            "text": "Lorem ipsum dolor sit amet consectetur adipisicing elit. Quisquam, voluptatum.",
-            "image": "/images/chicken.png",
-            "type": "dinner",
-        },
-        {
-            "name": "CAKE",
-            "price": 18,
-            "text": "Lorem ipsum dolor sit amet consectetur adipisicing elit. Quisquam, voluptatum.",
-            "image": "/images/cake.png",
-            "type": "breakfast",
-        },
-        {
-            "name": "BURGER",
-            "price": 23,
-            "text": "Lorem ipsum dolor sit amet consectetur adipisicing elit. Quisquam, voluptatum.",
-            "image": "/images/burger.png",
-            "type": "lunch",
-        },
-        {
-            "name": "PANCAKE",
-            "price": 25,
-            "text": "Lorem ipsum dolor sit amet consectetur adipisicing elit. Quisquam, voluptatum.",
-            "image": "/images/pancake.png",
-            "type": "dinner",
-        }
-    ]
-    
-    return JsonResponse(foodData, safe=False)
+class ProductListView(ListAPIView):
+    queryset = Product.objects.all()  # Set the queryset here
+    serializer_class = ProductSerializer
+    permission_classes = [DjangoModelPermissionsOrAnonReadOnly]  # You can keep this permission class if needed
