868137a2f9b24bc0c3d865b65a24ea6766ee3250
Abhijith Subash <abhijithsubash07@gmail.com>
2024-02-13 14:56:55 +0530

    fix wsgi production settings added

--- foody/wsgi.py
+++ foody/wsgi.py
@@ -2,7 +2,7 @@ import os
 
 from django.core.wsgi import get_wsgi_application
 
-os.environ.setdefault("DJANGO_SETTINGS_MODULE", "foody.settings")
+os.environ.setdefault("DJANGO_SETTINGS_MODULE", "foody.settings.production")
 
 application = get_wsgi_application()
 
